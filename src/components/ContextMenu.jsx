0156aa9cf9541c1ccfe2421dec0dd5a33d5dfeb6
shirish2121 <shirish.nigam@vartagram.com>
2025-10-03 22:38:16 +0530

    when the user right click on the rows of the table then the context menu (customized) opens with edit and delete option but edit and delete functionality needed to be implemented

--- src/components/ContextMenu.jsx
+++ src/components/ContextMenu.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-export default function ContextMenu() {
+export default function ContextMenu({ menuPosition }) {
+  if (!menuPosition.left) return;
   return (
-    <div className='context-menu'>
+    <div className='context-menu' style={menuPosition}>
       <div>Edit</div>
       <div>Delete</div>
     </div>
